27e12ee90709d9024740edb92ce5fb61d9feb528
AFTAN <aftanahmadskills@gmail.com>
2020-12-18 16:43:30 +0530

    Add files via upload

---
+++ Restaurant Site/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+  $('.food-slider').slick({
+    slidesToShow : 3,
+    slidesToScroll : 1,
+    prevArrow: ".prev-btn",
+    nextArrow: ".next-btn",
+    autoplay : true,
+  });
+});
